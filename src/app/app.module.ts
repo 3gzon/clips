988e7388389d690903a114cb40dd2801cd01f563
Egzon <egzonuka8@gmail.com>
2022-09-14 10:36:48 +0200

    feat: added shared folder for doing reusable components, in this case for modal component for auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { AuthModalComponent } from './user/auth-modal/auth-modal.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, AuthModalComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, NavComponent],
+  imports: [BrowserModule, AppRoutingModule, UserModule],
   providers: [],
   bootstrap: [AppComponent]
 })

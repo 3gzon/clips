5a41832a0befade89c0732975224ae988ba7cbff
Egzon <egzonuka8@gmail.com>
2022-09-15 12:23:19 +0200

    feat: use contentchildren decorator, and use ngaftercontentinit to find the number of tabs

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ContentChildren, AfterContentInit } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.scss']
+})
+export class TabsContainerComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs = {};
+  constructor() {}
+
+  ngAfterContentInit() {
+    console.log(this.tabs, 'test');
+  }
+}

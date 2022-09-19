e168d6d8d5cedc0c0d771fb95942842f41c2e2e5
Egzon <egzonuka8@gmail.com>
2022-09-19 22:38:58 +0200

    feat: worked reuasble component for input, did reusable formcontrols

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { ModalService } from '../services/modal.service';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
   //array of services
   providers: [ModalService]
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,7 +7,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  name = new FormControl('', [Validators.required]);
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('');
   age = new FormControl('');
   password = new FormControl('');

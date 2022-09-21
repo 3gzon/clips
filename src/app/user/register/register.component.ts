adc6be4f2a2ab4d5cdd3f76af5161b3d4a896240
Egzon <egzonuka8@gmail.com>
2022-09-21 14:19:37 +0200

    feat: sending response, include submitting form and register user to the firebase

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  constructor(private auth: AngularFireAuth) {}
+  inSubmission = false;
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('', [Validators.required, Validators.email]);
   age = new FormControl('', [Validators.required, Validators.min(18), Validators.max(120)]);
-  password = new FormControl('', [
-    Validators.pattern('/^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm')
-  ]);
+  password = new FormControl('', [Validators.required]);
   confirm_password = new FormControl('', Validators.required);
   phoneNumber = new FormControl('', [
     Validators.required,
@@ -33,9 +33,25 @@ export class RegisterComponent implements OnInit {
   });
 
   ngOnInit(): void {}
-  register() {
+  async register() {
     this.showAlert = true;
     this.alertMsg = 'Please wait! Your account has been created';
     this.alertColor = 'blue';
+    this.inSubmission = true;
+    const { email, password } = this.registerForm.value;
+    try {
+      const userCred = await this.auth.createUserWithEmailAndPassword(
+        email as string,
+        password as string
+      );
+    } catch (e) {
+      console.error(e);
+      this.alertMsg = 'An unexcepted error';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+    this.alertMsg = 'Success, your account has been created';
+    this.alertColor = 'green';
   }
 }
